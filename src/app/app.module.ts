a660d36b54abd67b90fa60c98c85dcb2bc7c77eb
Ashish Guhe <ashguhe@gmail.com>
2020-09-20 00:02:50 -0400

    Video 18: Added component code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.coponent';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
